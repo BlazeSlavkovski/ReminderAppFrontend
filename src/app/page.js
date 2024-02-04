f84181d4af06560ad7262ab0cf8dec505978285b
Blaze Slavkovski <Blazeslavkovski@gmail.com>
2024-02-04 17:57:20 -0500

    inital commit of the frontend

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,134 @@
-import Image from "next/image";
+"use client" 
+import { useState, useEffect } from "react";
 import styles from "./page.module.css";
+import axios from 'axios'
+import { Button } from "react-bootstrap";
+import LoginForm from "./components/loginForm/loginForm";
+import RegisterForm from "./components/registerForm/registerForm";
+import NavBar from "./components/Navbar/navBar";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import TaskPage from "./components/TaskPage/taskPage";
+
+const client = axios.create({
+  baseURL:'http://127.0.0.1:8000',
+  withCredentials:true
+})
+
 
 export default function Home() {
+
+  const[loggedIn,setLoggedIn] = useState()
+  const[toggleRegisterPage,setToggleRegisterPage] = useState(false)
+  const[email,setEmail] = useState('')
+  const[username,setUsername] = useState('')
+  const[password,setPassword] = useState('')
+  const[userDetails, setUserDetails] = useState()
+
+  useEffect(()=> {
+    async function fetchMyAPI() {
+      try{
+        let response = await client.get('api/user')
+        setUserDetails(response.data)
+        setLoggedIn(true)
+      }catch(error){
+        console.log(error)
+        setUserDetails()
+      }
+    }
+
+    fetchMyAPI()
+  },[loggedIn])
+
+  const toggleRegister = () => {
+    setToggleRegisterPage(!toggleRegisterPage)
+  }
+
+  const handleRegistration = async(e) => {
+    e.preventDefault()
+    try{
+      await client.post(
+        'api/register',{
+          email:email,
+          name:username,
+          password:password
+        }
+      )
+      await client.post(
+        'api/login',{
+          email:email,
+          password:password
+        }
+      )
+      setLoggedIn(true)
+    }catch(error){
+      console.log(error)
+    }
+  } 
+
+  const handleLogin = async(e) => {
+    e.preventDefault()
+    try{
+      await client.post(
+        'api/login',{
+          email:email,
+          password:password
+        },{withCredentials:true}
+      )
+      setLoggedIn(true)
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+  const handleLogout = async(e) => {
+    e.preventDefault()
+    try{
+     await client.post('api/logout')
+      setLoggedIn(false)
+    }catch(error){
+      console.log(error)
+    }
+  }
+
   return (
+  <div>
+    <NavBar handleLogout={handleLogout} userDetails={userDetails}/>
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    
+      {loggedIn 
+      ? <TaskPage/>
+      : (toggleRegisterPage
+          ? <RegisterForm 
+              email={email} 
+              setEmail={setEmail} 
+              username={username} 
+              setUsername={setUsername} 
+              password={password} 
+              setPassword={setPassword} 
+              handleRegistration={handleRegistration}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          : <LoginForm 
+              email={email} 
+              setEmail={setEmail} 
+              password={password} 
+              setPassword={setPassword} 
+              handleLogin={handleLogin}
+            />
+        )
+      }
+          {!userDetails && 
+            <div className="justify-content-between my-4">
+              <p>{toggleRegisterPage ? 'Already have an account?' : "Don't have an account?"}</p>
+              <Button onClick={toggleRegister} variant="link" className="mx-4">
+                {toggleRegisterPage ? 'Login' : 'Register'}
+              </Button>
+              </div>
+          }
     </main>
+  </div>
+    
   );
 }
